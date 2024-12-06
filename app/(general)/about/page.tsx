1a0c2faf1e8a23ac5944b3399db0aa75f42d85e4
Nicolas <nvergararojas53@gmail.com>
2024-12-06 17:11:26 -0500

    first commit

--- app/(general)/about/page.tsx
+++ app/(general)/about/page.tsx
@@ -6,6 +6,6 @@ export const metadata: Metadata = {
   keywords: ["About", "Page"],
 };
 
-export default function AboutPage() {
+export default function AboutPage() { 
   return <span className="text-7xl">About page</span>;
 }
